cb500ecb76ed6dcf3a3b90464c647e4e80e77259
Mikita Kryvitski <solairerove@gmail.com>
2019-07-08 21:28:18 +0300

    feature: jedi lvl 4 ex 1

--- jedi/ex_1.go
+++ jedi/ex_1.go
@@ -7,4 +7,19 @@ import (
 // Ex1 fmt
 func Ex1() {
 	fmt.Println("Ex1")
+
+	var x [5]int
+	x[0] = 0
+	x[1] = 1
+	x[2] = 2
+	x[3] = 3
+	x[4] = 4
+
+	fmt.Println(x)
+
+	for _, v := range x {
+		fmt.Println(v)
+	}
+
+	fmt.Printf("%T\n", x)
 }
